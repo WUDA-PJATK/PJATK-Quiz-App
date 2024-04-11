598edea221b68312ae83b7083706f39da01c0ae9
Michal <michal.zag@o2.pl>
2024-04-11 15:06:42 +0200

    answer implementation

---
+++ quizAppBackend/quizAppBackend/src/main/java/com/quizApp/quizApp/Pytania/AnswerRepository.java
@@ -0,0 +1,8 @@
+package com.quizApp.quizApp.Pytania;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+}
