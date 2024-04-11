24c3cd3e04cb3ce4a94ab48d568f46a6ba9948ee
opaaax <opaaaax@gmail.com>
2024-04-11 14:56:56 +0200

    minor changes, connecting to DB

--- quizAppBackend/quizAppBackend/src/main/java/com/quizApp/quizApp/Pytania/Answer.java
+++ quizAppBackend/quizAppBackend/src/main/java/com/quizApp/quizApp/Pytania/Answer.java
@@ -2,12 +2,14 @@ package com.quizApp.quizApp.Pytania;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
 @Entity
+@Table
 public class Answer {
     @Id
     private Long id;
